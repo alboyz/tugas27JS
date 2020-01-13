0414be7955936c467f25e421b02f0a162654460b
ali <alboyzskeaters@gmail.com>
2020-01-13 14:19:04 +0700

    Tugas27JS

---
+++ tugas27.js
@@ -0,0 +1,6 @@
+let kalimat1 = ["No", "rumah saya", 30, "A"];
+let jkalimat1 = kalimat1.join(" ").toString();
+console.log(jkalimat1);
+let kalimat2 = ["Jalan pangeran Tirtayasa No", 70];
+let jkalimat2 = kalimat2.join(" ").toString();
+console.log(jkalimat2);
